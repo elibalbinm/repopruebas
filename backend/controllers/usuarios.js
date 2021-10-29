dec375fe9936c7f55bb8c46ebc6353d24fe6edf8
elibalbinm <eibm1@alu.ua.es>
2021-10-29 20:41:37 +0200

    POST: comprobar email, cifrar contraseña y almacenar en BBDD

--- backend/controllers/usuarios.js
+++ backend/controllers/usuarios.js
@@ -1,6 +1,9 @@
 const Usuario = require('../models/usuarios');
 const { validationResult } = require('express-validator');
-const getUsuarios = async(req, res) => {
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+
+const obtenerUsuarios = async(req, res) => {
 
     const usuarios = await Usuario.find({});
     // const usuarios = await Usuario.find({}, 'nombre apellidos');
@@ -11,21 +14,70 @@ const getUsuarios = async(req, res) => {
     });
 }
 
-const crearUsuarios = async(req, res) => {
+const crearUsuario = async(req, res = response) => {
     const erroresVal = validationResult(req);
+    const { email, password, rol } = req.body;
+
+    // VALIDACIONES
+    try {
+        const existeEmail = await Usuario.findOne({ email: email });
+
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Email ya existe'
+            });
+        }
+
+        if (rol && rol != 'ROL_ALUMNO' && rol != 'ROL_PROFESOR' && rol != 'ROL_ADMIN') {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Rol incorrecto'
+            });
+        }
+
+        const salt = bcrypt.genSaltSync();
+        const cpassword = bcrypt.hashSync(password, salt);
+
+        // Almacenar en la base de datos
+        const usuario = new Usuario(req.body);
+        usuario.password = cpassword;
+
+        await usuario.save();
+
+        res.json({
+            ok: true,
+            msg: 'crearUsuarios',
+            usuario
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({
+            ok: false,
+            msg: 'Error creando usuario'
+        });
+    }
+
     if (!erroresVal.isEmpty()) {
         return res.status(400).json({
             ok: false,
             errores: erroresVal.mapped()
         });
     }
+}
+
+const actualizarUsuario = async(req, res) => {
     res.json({
         ok: true,
-        msg: 'crearUsuarios'
+        msg: 'actualizarUsuario'
+    });
+}
+const borrarUsuario = async(req, res) => {
+    res.json({
+        ok: true,
+        msg: 'borrarUsuario'
     });
 }
 
-module.exports = {
-    getUsuarios,
-    crearUsuarios
-}
+module.exports = { obtenerUsuarios, crearUsuario, actualizarUsuario, borrarUsuario }
--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -2,17 +2,39 @@
 // /api/usuarios
 
 const { Router } = require('express');
-const { getUsuarios, crearUsuarios } = require('../controllers/usuarios');
+const { obtenerUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middleware/validar-campos');
 
 const router = Router();
 
-router.get('/', getUsuarios);
+router.get('/', obtenerUsuarios);
 // router.post('/', crearUsuarios);
+// router.post('/', [
+//     check('nombre', 'El argumento nombre es obligatorio').not().isEmpty(),
+//     validarCampos
+// ], crearUsuarios);
+
 router.post('/', [
     check('nombre', 'El argumento nombre es obligatorio').not().isEmpty(),
+    check('apellidos', 'El argumento apellidos es obligatorio').not().isEmpty(),
+    check('email', 'El argumento email es obligatorio').not().isEmpty(),
+    check('password', 'El argumento password es obligatorio').not().isEmpty(),
     validarCampos
-], crearUsuarios);
+], crearUsuario);
+
+router.put('/:id', [
+    check('nombre', 'El argumento nombre es obligatorio').not().isEmpty(),
+    check('apellidos', 'El argumento apellidos es obligatorio').not().isEmpty(),
+    check('email', 'El argumento email es obligatorio').not().isEmpty(),
+    check('password', 'El argumento password es obligatorio').not().isEmpty(),
+    check('id', 'El identificador no es válido').isMongoId(),
+    validarCampos
+], actualizarUsuario);
+
+router.delete('/:id', [
+    check('id', 'El identificador no es válido').isMongoId(),
+    validarCampos
+], borrarUsuario);
 
 module.exports = router;
